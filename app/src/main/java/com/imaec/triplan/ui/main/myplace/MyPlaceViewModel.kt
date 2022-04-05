cb4380e7623163635b994bf7d22220387f154da3
imaec <dev.imaec@gmail.com>
2022-04-05 19:14:22 +0900

    [내 장소] 내 장소 화면 UI 구현

--- app/src/main/java/com/imaec/triplan/ui/main/myplace/MyPlaceViewModel.kt
+++ app/src/main/java/com/imaec/triplan/ui/main/myplace/MyPlaceViewModel.kt
@@ -3,6 +3,7 @@ package com.imaec.triplan.ui.main.myplace
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.imaec.domain.model.PlaceDto
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -11,6 +12,9 @@ class MyPlaceViewModel @Inject constructor() : ViewModel() {
 
     private val _state = MutableLiveData<MyPlaceState>()
     val state: LiveData<MyPlaceState> get() = _state
+
+    private val _placeList = MutableLiveData<List<PlaceDto>>()
+    val placeList: LiveData<List<PlaceDto>> get() = _placeList
 
     fun onClickWrite() {
         _state.value = MyPlaceState.OnClickWrite
